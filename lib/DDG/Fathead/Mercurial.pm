316f6db4b8bde25d6c71f1832c5f1081b3fb84dc
Chris Mansley <cmansley@cs.rutgers.edu>
2013-03-02 22:01:03 -0800

    Add perl meta information file

---
+++ lib/DDG/Fathead/Mercurial.pm
@@ -0,0 +1,28 @@
+package DDG::Fathead::Mercurial;
+
+use DDG::Fathead;
+
+primary_example_queries "hg commit";
+
+secondary_example_queries
+    "mercurial commit";
+
+description "Mercurial command reference";
+
+name "Mercurial";
+
+icon_url "/i/www.github.com.ico";
+
+source "Mercurial";
+
+code_url "https://github.com/duckduckgo/zeroclickinfo-fathead/tree/master/share/mercurial";
+
+topics "geek", "sysadmin", "programming";
+
+category "reference";
+
+attribution
+    github => ['https://github.com/boyter', 'boyter'],
+    web => ['http://chrismansley.com', 'Chris Mansley'];
+
+1;
